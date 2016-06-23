3fd3e4d6adb84b6abf438d99e567f318e9ab782d
Mohamad Sholihin <mohamad.sholihin.it@gmail.com>
2016-06-23 23:37:21 +0700

    remove ukobag

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <div class="about">
 <h2>KONTAK KAMI</h2>
 <p style="font-size:13px">
-Ukobag akan memberikan pengalaman belanja online yang terbaik. Kami akan senang untuk membantu menjawab setiap bisnis, pertanyaan komersial dan umum yang Anda miliki. Kirim email, dan kami akan mencoba untuk membalas sesegera mungkin.
+Clutch akan memberikan pengalaman belanja online yang terbaik. Kami akan senang untuk membantu menjawab setiap bisnis, pertanyaan komersial dan umum yang Anda miliki. Kirim email, dan kami akan mencoba untuk membalas sesegera mungkin.
 <br />
 <div style="letter-spacing:1px;text-align:center;font-size:13px;">Email : <b>cs@clutch.com</b><br>
 Instagram : <b>@clutch</b>
